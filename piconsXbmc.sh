587e6d14c043992d5b0adadd49695de0cb7318f9
Sebastian Hardt <s.hardt@micromata.de>
2012-11-09 16:34:38 +0100

    changed some stuff

--- piconsXbmc.sh
+++ piconsXbmc.sh
@@ -1,12 +1,26 @@
 #!//bin/bash
 
-#update the icons
+#update the script folder for any changes
 git pull;
 
+# initialize the submodules
+git submodule init
+
+# update the submoudles
+git submodule update
+
+# create picons symlinks
+sh ./picons/picons.sh ./picons/picons
+
+
+# create the icons folder
 mkdir -p xbmc_icons;
 
+# clean out
 rm xbmc_icons/*
 
-python2 xbmcIconLinks.py -c channels.conf -m c > createXbmcIonsLinks.sh
+# call the python script in copy mode
+python2 xbmcIconLinks.py -c channels.conf -p ./picons/picons/ -m c > createXbmcIonsLinks.sh
 
+# run the sh
 sh createXbmcIonsLinks.sh 
