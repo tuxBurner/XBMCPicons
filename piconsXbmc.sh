5f8247b16cc3c8f99dc387a74f0c0645259d38fe
tuxburner <sebasth@gmx.de>
2013-03-20 09:13:26 +0100

    fixed submodules stuff

--- piconsXbmc.sh
+++ piconsXbmc.sh
@@ -12,19 +12,21 @@ git submodule foreach git pull origin master
 # update the submoudles
 #git submodule update
 
+# rem old picons symlinks
+rm ./picons/picons/1_0*.png
 
 # create picons symlinks
 sh ./picons/picons.sh ./picons/picons
 
 
 # create the icons folder
-mkdir -p xbmc_icons;
+mkdir -p xbmc_icons
 
 # clean out
-rm xbmc_icons/*
+rm xbmc_icons/*.png
 
 # call the python script in copy mode
-#ython2 xbmcIconLinks.py -p ./picons/picons/ -m c > createXbmcIonsLinks.sh
+python2 xbmcIconLinks.py -p ./picons/picons/ -m c > createXbmcIonsLinks.sh
 
 # run the sh
 sh createXbmcIonsLinks.sh 
