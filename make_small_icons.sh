6b9b93ba86b574db5446164f084fc37ac4629611
Seppel Hardt <sebasth@gmx.de>
2014-06-24 22:59:17 +0200

    Small icons with background now woohohh

--- make_small_icons.sh
+++ make_small_icons.sh
@@ -16,6 +16,15 @@ cp xbmc_icons/*.svg xbmc_icons_small/
 
 # make them small :)
 cd xbmc_icons_small
-mogrify -resize 128x128 *.png
-mogrify -format png -resize 128x128 *.svg
+mogrify -resize 250x250 *.png
+mogrify -format png -resize 250x250 *.svg
+rm *.svg
+mkdir backgrounded
+for f in *.png; do
+  echo "Processing $f file..";
+  convert ../picons/backgrounds/xvdr/reflection-black.png "$f" -gravity center -composite -resize 128X128 "backgrounded/$f"
+done
+rm *.png
+mv backgrounded/*.png ./
+rm -rf backgrounded
 cd ..
